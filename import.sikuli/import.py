32a6d4865b92fa604a289965371c06e4ea62df96
farahabbasi <farahabbasi03@gmail.com>
2014-11-29 12:33:02 -0500

    creting an import test for #255

---
+++ import.sikuli/import.py
@@ -0,0 +1,11 @@
+find("1417281108574.png")
+doubleClick("1417281089525.png")
+type("teammatetiger\tphoneme\n")
+wait("1417281245397.png")
+find(Pattern("1417281964503.png").targetOffset(3,-7))
+doubleClick(Pattern("1417282150528.png").targetOffset(32,-8))
+
+
+doubleClick(Pattern("1417281267711.png").targetOffset(33,-17))
+
+
