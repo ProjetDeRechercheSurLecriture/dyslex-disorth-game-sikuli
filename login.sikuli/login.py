9e8d2f50f99ab0e3afbdbb5734a28784274ca392
farahabbasi <farahabbasi03@gmail.com>
2014-11-29 23:58:18 -0500

    all tests

---
+++ login.sikuli/login.py
@@ -0,0 +1,7 @@
+find("1417281108574.png")
+doubleClick("1417281089525.png")
+type("teammatetiger\tphoneme\n")
+wait("1417281245397.png")
+doubleClick(Pattern("1417281267711.png").targetOffset(33,-17))
+
+
